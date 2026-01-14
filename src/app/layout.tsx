66dd5c5628fef1c7b69ec94312eade8de60802cd
Falquito <joelserrudo@gmail.com>
2026-01-14 18:39:58 -0300

    base-project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,26 +2,10 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "CareLink",
-  description: "CareLink Application",
-  icons: {
-    icon: [
-      { url: "/favicon.ico", sizes: "48x48", type: "image/x-icon" },
-      { url: "/favicon.ico", sizes: "32x32", type: "image/x-icon" },
-      { url: "/favicon.ico", sizes: "16x16", type: "image/x-icon" },
-    ],
-    shortcut: "/favicon.ico",
-    apple: "/favicon.ico",
-    other: [
-      {
-        rel: 'icon',
-        type: 'image/x-icon',
-        sizes: '256x256',
-        url: '/favicon.ico',
-      },
-    ],
-  },
-};
+  title: "Dermacor",
+  description: "Dermacor Application",
+
+}
 
 export default function RootLayout({
   children,
